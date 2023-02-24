28f3e3bbb1b5f8c434258a08f1a6bd0d24a88b0c
Krishna Gupta <krishnagupta09030903@gmail.com>
2023-02-24 20:59:21 +0530

    Initial Commit

---
+++ app/src/main/java/com/reallykrishna/minutecalculator/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.reallykrishna.minutecalculator
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var tvSelectedDate : TextView? = null
+    private var tvAgeInMinutes : TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
+
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+    private fun clickDatePicker(){ // This function can be executed in this class only.
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{ view, selectedYear, selectedMonth, selectedDayOfMonth ->
+
+                Toast.makeText(this, "Year was $selectedYear, Month was ${selectedMonth+1}, " +
+                        "Day was $selectedDayOfMonth", Toast.LENGTH_LONG ).show()
+
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+                tvSelectedDate?.text = selectedDate
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val theDate = sdf.parse(selectedDate)
+
+                theDate?.let{ //If theDate is not empty then execute the following:-
+                    val selectedDateInMinutes = theDate.time/60000
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
+                    currentDate?.let{ // If currentDate is not empty then execute the following:-
+                        val currentDateInMinutes = currentDate.time/60000
+                        val differenceInMinutes = currentDateInMinutes-selectedDateInMinutes
+
+                        tvAgeInMinutes?.text = differenceInMinutes.toString()
+                    }
+                }
+            },
+            year,
+            month,
+            day
+        )
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
+    }
+}
